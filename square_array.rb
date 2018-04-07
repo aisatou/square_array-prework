38b7f17e96de550c5ff093ec1f5bb87bb304e39e
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-04-07 01:24:35 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
-def square_array(array)
-  arr = []
-  array.each { |x| arr << x ** 2 }
-  arr
-end
+#def square_array(array)
+  #arr = []
+  #array.each { |x| arr << x ** 2 }
+  #arr
+#end
 
 def square_array(array)
   new_array = []
